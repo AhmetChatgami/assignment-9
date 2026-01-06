2780665b6cb49246465d017138a8558ca661f0df
AhmedYeasin <ahmetchatgami@gmail.com>
2026-01-06 18:15:21 +0600

    description added for products

--- src/Pages/ToysDetails.jsx
+++ src/Pages/ToysDetails.jsx
@@ -7,8 +7,8 @@ const ToysDetails = () => {
   console.log(toys);
   const { id } = useParams();
   console.log(id);
-  if(loading){
-    return <p>loading...</p>
+  if (loading) {
+    return <p>loading...</p>;
   }
 
   const toy = toys.filter((p) => Number(p.toyId) == id);
@@ -21,35 +21,41 @@ const ToysDetails = () => {
     rating,
     availableQuantity,
     pictureURL,
-    toyId, subCategory
+    toyId,
+    subCategory, description, sellerEmail
   } = toy[0];
   console.log(toy);
   return (
- 
-   <div>
-      <h2>Toys Details Page</h2>
-      <div className="card card-side bg-base-100 shadow-sm">
-        <figure>
-          <img className="w-70"
-            src={pictureURL}
-            alt="toys"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{toyName}</h2>
-          <p>{sellerName}</p>
-          <p>Toy Category: {subCategory}</p>
-          <p>Price: ${price}</p>
-          <p>Rating: <span className="bg-amber-400 p-1 rounded-full">{rating}</span></p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">{sellerName}</button>
+    <div>
+      <div>
+        <h2>Toys Details Page</h2>
+        <div className="card card-side bg-base-100 shadow-sm">
+          <figure>
+            <img className="w-70" src={pictureURL} alt="toys" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">{toyName}</h2>
+            <p>{sellerName}</p>
+            <p>Toy Category: {subCategory}</p>
+            <p>Price: ${price}</p>
+            <p>
+              Rating:{" "}
+              <span className="bg-amber-400 p-1 rounded-full">{rating}</span>
+            </p>
+            <p className="font-semibold">Contact: {sellerEmail}</p>
+            
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">{sellerName}</button>
+            </div>
           </div>
         </div>
       </div>
+     <div className="py-10">
+        <h2 className=" text-2xl font-semibold">Description:</h2>
+         <p className="text-gray-500 pt-4">{description}</p>
+     </div>
     </div>
-
-  )
-  
+  );
 };
 
 export default ToysDetails;
