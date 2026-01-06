b824b5eea89a2ff607e7836c86969e6d689cfa4c
AhmedYeasin <ahmetchatgami@gmail.com>
2026-01-06 12:08:13 +0600

    Hooks created & files edited.

--- src/Pages/ToysGallery.jsx
+++ src/Pages/ToysGallery.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ToysGallery = () => {
-  return <h2>all toys will be here</h2>;
+  return <h2>Toys Gallery; all toys will be here</h2>;
 };
 
 export default ToysGallery;
